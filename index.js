05561a89fd9d44846bb0f98b602738061059d2e3
Joseph bayad <jobayad66@gmail.com>
2021-06-21 21:52:17 +0100

    create pot page

--- index.js
+++ index.js
@@ -19,14 +19,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+  res.render('about');
 });
 app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+  res.render('contact');
 });
 
 app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+  res.render('post');
+});
+
+app.get('/posts/new', (req, res) => {
+  res.render('create');
 });
 
 app.listen(4000, () => {

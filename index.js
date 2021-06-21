b953a67de8d032fb924f82c63cbe7133bf6c7070
Joseph bayad <jobayad66@gmail.com>
2021-06-21 16:08:19 +0100

    ejs template engne

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
+import ejs from 'ejs';
+
 const app = express();
+app.set('view engine', 'ejs');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,7 +15,7 @@ mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
